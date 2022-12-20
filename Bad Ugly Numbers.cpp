a0d33cea539eacaed6ebf56d9af63ac2826e14c0
Md. Yusuf Hossen <112095275+hereimyusuf@users.noreply.github.com>
2022-12-20 21:13:47 +0600

    Add files via upload

---
+++ Bad Ugly Numbers.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/contest/1326/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+string iNeed9s(int n) {
+    string s="";
+    while(n--) s+='9';
+    return s;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int t,n;
+    cin>>t;
+    while(t--) cin>>n,n==1?cout<<-1<<endl:cout<<2<<iNeed9s(n-1)<<endl;
+}
