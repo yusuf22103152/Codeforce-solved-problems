7c4c6be607a5de3c8a9908c117047fd1eacb603a
Md. Yusuf Hossen <112095275+hereimyusuf@users.noreply.github.com>
2023-01-27 12:55:25 +0600

    Add files via upload

---
+++ Find Divisible.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/contest/1096/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int l,r;
+        cin>>l>>r;
+        cout<<l<<" "<<2*l<<endl;
+    }
+}
