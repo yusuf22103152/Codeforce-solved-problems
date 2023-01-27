7dbfcae1391e358a387e6cc8b7197eaaa67b8faf
Md. Yusuf Hossen <112095275+hereimyusuf@users.noreply.github.com>
2023-01-27 12:56:04 +0600

    Add files via upload

---
+++ Prime Subtraction.cpp
@@ -0,0 +1,9 @@
+//https://codeforces.com/contest/1238/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int t,x,y,z;
+    cin>>t;
+    while(t--) cin>>x>>y,x-y==1?cout<<"nO\n":cout<<"yeS\n";
+}
